6ce8e5bf535bf5a188667e8fcebbe570b53b2ea2
rishabkhanna26 <rishabkhanna26@gmail.com>
2025-06-26 15:36:47 +0530

    loader issue fix & added next js as skill

--- script.js
+++ script.js
@@ -175,12 +175,12 @@ $(function () {
               margin-bottom: 8px;
               background: linear-gradient(135deg, #9573D6 60%, #7B4BC3 100%);
               border-radius: 50%;
-              padding: 10px;
+              padding: 32px;
               box-shadow: 0 1px 8px rgba(149,115,214,0.13);
               object-fit: contain;
               border: 2px solid #fff2;
-              width:  3.5rem;
-              height: 3.5rem;
+              width:  8.5rem;
+              height: 8.5rem;
             ">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" width="100%" height="100%" style="display:block;">
                 <path d="M64 0C28.7 0 0 28.7 0 64s28.7 64 64 64c11.2 0 21.7-2.9 30.8-7.9L48.4 55.3v36.6h-6.8V41.8h6.8l50.5 75.8C116.4 106.2 128 86.5 128 64c0-35.3-28.7-64-64-64zm22.1 84.6l-7.5-11.3V41.8h7.5v42.8z" fill="#fff"/>
